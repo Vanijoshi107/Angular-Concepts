fd6a7f473cd6335c8486917eb43c16e84d3fd43a
Vanijoshi107 <anyajoshi20@gmail.com>
2021-10-17 22:56:15 +0530

    event and two way binding

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  btnColor:string = "btn "+"purple";
+
+  salary:number = 100;
+  convertValue(){
+    alert(this.salary*75)
+  }
+
+// event +property binding -- two way binding
+  inputValue:string= "Mango"
+
+  getInput(e:any){
+    // console.log(e.target.value)
+    this.inputValue = e.target.value;
+  }
+}
