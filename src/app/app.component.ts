1e2a5a40c40f1e9304d4aac8a68e1598a65a5fe0
Vanijoshi107 <anyajoshi20@gmail.com>
2021-10-23 19:14:26 +0530

    user models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models/users';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mastercourse';
-  users=[
+  title:string = 'Mastercourse';
+  users:Array<User>=[ // defining the type of users
     {name:"abc", age:23,salary:35000,gender:"female", imgPath:"assets/person1.webp"},
     {name:"pqr", age:23,salary:30000,gender:"female", imgPath:"assets/person2.webp"},
     {name:"mno", age:23,salary:45000,gender:"female", imgPath:"assets/person3.webp"}
+    // { by clicking ctrl+space we can check the suggestions... it will be same as we made in models. }
   ]
 
   //making a function
---
+++ src/models/users.ts
@@ -0,0 +1,8 @@
+export interface User{
+    name:string,
+    age:number,
+    salary:number,
+    gender:string,
+    imgPath:string
+}
+//this way we made our own datatypes.
